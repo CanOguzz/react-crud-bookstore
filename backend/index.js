8346ec0debb968f5f4a85cb54a1d797cb43c5553
CanOguzz <mustafacanoguz@gmail.com>
2024-03-14 11:00:01 +0300

    Add mysql dependency and create connection to the database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mysql from 'mysql'
 
 const app = express();
+
+const dp=mysql.createConnection({
+host:"localhost",
+user:"root",
+password:"admin",
+database:"test1"
+})
 
 app.listen(8800, () => {
     console.log('Backend server is running!');
