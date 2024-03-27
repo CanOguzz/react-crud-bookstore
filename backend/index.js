5ed5ddae545c2916e28634d88e932fff57daf331
CanOguzz <mustafacanoguz@gmail.com>
2024-03-27 16:02:08 +0300

    Refactor backend/index.js file add commend lines

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 import express, { json } from 'express';
 import mysql from 'mysql'
 import cors from 'cors';
-
+// Log a message to the console when the server starts running
 const app = express();
-
+// Create a connection to the MySQL database
 const db=mysql.createConnection({
 host:"localhost",
 user:"root",
@@ -11,15 +11,15 @@ password:"admin",
 database:"test1"
 })
 
-//allow us to send any json file as client
+// Middleware to parse JSON bodies from HTTP requests
 app.use(express.json());
-
+// Middleware to enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
-
+// Define a GET route for the root path
 app.get('/', (req, res) => {
     res.json(   "this is backend "  );
 })
-
+// Define a GET route to fetch all books
 app.get('/books', (req, res) => {
     const q="SELECT * FROM books";
     db.query(q,(err,data)=>{
@@ -28,7 +28,7 @@ app.get('/books', (req, res) => {
     })
         
 });
-
+// Define a POST route to add a new book
 app.post('/books', (req, res) => {
     const q="INSERT INTO books (`title`, `desc`,`price`,`cover`) VALUES (?)";
     const values=
@@ -42,7 +42,7 @@ app.post('/books', (req, res) => {
         return res.json("book hass been added successfully");
     })
 });
-
+// Define a DELETE route to delete a book by ID
 app.delete('/books/:id', (req, res) => {
     const bookId = req.params.id;
 
@@ -53,11 +53,11 @@ app.delete('/books/:id', (req, res) => {
     })
 });
 
-
+// Start the server on port 8800
 app.listen(8800, () => {
     console.log('Backend server is running!');
   });
-
+// Define a PUT route to update a book by ID
   app.put("/books/:id", (req, res) => {
     const bookId = req.params.id;
     const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
