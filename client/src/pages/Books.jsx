ccbbe29a4b230c6ead90d6b7836bd481cfc7674f
CanOguzz <mustafacanoguz@gmail.com>
2024-03-27 16:11:56 +0300

    add commend lines

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -4,14 +4,15 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
+// Define the Books component
 const Books = () => {
-
+  //Initialize the books state variable
   const [books, setBooks] = useState([])
-  
+  // Define a useEffect hook to fetch all books when the component mounts
   useEffect(() => {
   const fetchAllBooks = async () => {
     try{
+       // Send a GET request to the server to fetch all books
       const res = await axios.get("http://localhost:8800/books");
       console.log(res);
       setBooks(res.data);
@@ -19,18 +20,21 @@ const Books = () => {
       console.error(err.message);
     }
   }
+   // Call the fetchAllBooks function
      fetchAllBooks();
   }, []);
-
+// Define a function to handle deleting a book
 const handleDelete = async (id) => {
   try{
+     // Send a DELETE request to the server to delete the book
      await axios.delete(`http://localhost:8800/books/${id}`);
-    
+    // Reload the page after the book is deleted
     window.location.reload();
   }catch(err){
     console.error(err.message);
   }
 }
+// Render the books
   return <div>
     <h1>MCO Book Shop</h1>
     <div className="books">
